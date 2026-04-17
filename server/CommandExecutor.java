ee50a42ee049bfdc4452d5065ad0b798462df5fd
aleksandraTikhomirova <sashatihomirova16@gmail.com>
2026-04-17 20:08:09 +0300

    Add files via upload

---
+++ server/CommandExecutor.java
@@ -0,0 +1,56 @@
+package server;
+import com.sun.net.httpserver.Request;
+import common.commands.Command;
+import common.network.Response;
+import common.network.Request;
+import server.commands.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class CommandExecutor {
+    private final TicketCollection collection;
+    private final Map<String, Command> commands = new HashMap<>();
+
+    public CommandExecutor(TicketCollection collection){
+        this.collection=collection;
+        addAllCommands();
+    }
+    private void addAllCommands(){
+        commands.put('help',new HelpCommand());
+        commands.put('info',new InfoCommand());
+        commands.put('show',new ShowCommand());
+        commands.put('add',new AddCommand());
+        commands.put('update',new UpdateCommand());
+        commands.put('remove_by_id',new RemoveByIDCommand());
+        commands.put('clear',new ClearCommand());
+        commands.put('head',new HeadCommand());
+        commands.put('remove_head',new RemoveHeadCommand());
+        commands.put('remove_greater',new RemoveGreaterCommand());
+        commands.put('remove_all_by_price',new RemoveAllByPriceCommand());
+        commands.put('remove_any_by_type',new RemoveAnyByTypeCommand());
+        commands.put('min_by_venue',new MinByVenueCommand());
+        commands.put('save',new SaveCommand());
+    }
+    public Response execute(Request request){
+        String commandName = request.getCommandName();
+        Command command = commands.get(commandName);
+        if (command==null){
+            return new Response(false, "Неизвестная команда: "+ commandName)
+        }
+        try {
+            Object extraData = null;
+            if(command.requiresTicket()){
+                extraData = request.getTicket();
+            } else if (request.getPrice()!=null){
+                extraData = request.getPrice();
+            } else if (request.getType()!=null){
+                extraData = request.getType();
+            }
+            return command.execute(collection,request.getArg(),extraData);
+        } catch (Exception e){
+            return new Response(false, "Ошибка выполнения: "+e.getMessage());
+        }
+    }
+
+}
---
+++ server/TicketCollection.java
@@ -0,0 +1,184 @@
+package server;
+import common.data.*;
+import to_use.*;
+import server.util.Parser;
+import server.util.CreateID;
+import java.util.*;
+import java.time.LocalDateTime;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+public class TicketCollection {
+
+    private ArrayDeque<Ticket> collection;
+
+    private LocalDateTime date;
+
+    public TicketCollection() {
+        this.collection = new ArrayDeque<>();
+        this.date = LocalDateTime.now();
+    }
+
+    public TicketCollection(ArrayDeque<Ticket> firstCollection) {
+        this.collection = firstCollection;
+        this.date = LocalDateTime.now();
+        sortCollection();
+    }
+
+    private void sortCollection() {
+        collection = collection.stream().sorted().collect(Collectors.toCollection(ArrayDeque::new));
+    }
+
+    public Ticket head() {
+        return collection.peekFirst();
+    }
+
+    public void addElement(Ticket ticket) {
+        collection.add(ticket);
+        sortCollection();
+    }
+
+    public boolean update(int id, Ticket newTicket){
+        Iterator<Ticket> iterator = collection.iterator();
+        while (iterator.hasNext()){
+            Ticket ticket = iterator.next();
+            if (id == ticket.getId()){
+                iterator.remove();
+                CreateID.removeTicketID(id);
+                if (ticket.getVenue()!=null){
+                    CreateID.removeVenueID(ticket.getVenue().getID());
+                }
+                collection.add(newTicket);
+                sortCollection();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearCollection() {
+        for (Ticket ticket : collection) {
+            CreateID.removeTicketID(ticket.getId());
+            if (ticket.getVenue() != null) {
+                CreateID.removeVenueID(ticket.getVenue().getID());
+            }
+        }
+        collection.clear();
+    }
+
+    public String getInfo(){
+        return String.format("Тип коллекции: %s\nДата инициализации: %s\nРазмер коллекции: %d",
+                collection.getClass().getSimpleName(), date, collection.size());
+    }
+
+    public String showAll(){
+        if (collection.isEmpty()) return "Коллекция пустая";
+        return  collection.stream().sorted((t1,t2)->{
+            String loc1="";
+            if (t1.getVenue()!=null && t1.getVenue().getAddress() !=null &&
+            t1.getVenue().getAddress().getTown() != null &&
+            t1.getVenue().getAddress().getTown().getName() != null){
+                loc1= t1.getVenue().getAddress().getTown().getName();
+            }
+            String loc2="";
+            if (t2.getVenue()!=null && t2.getVenue().getAddress() !=null &&
+                    t2.getVenue().getAddress().getTown() != null &&
+                    t2.getVenue().getAddress().getTown().getName() != null){
+                loc2= t2.getVenue().getAddress().getTown().getName();
+            }
+            return loc1.compareTo(loc2);
+        }).map(Ticket::toString).collect(Collectors.joining("\n"));
+    }
+
+    public ArrayDeque<Ticket> getCollection() {return collection;}
+
+    public int size() {return collection.size();}
+
+    public void removeHead(){
+        Ticket ticket = head();
+        if (ticket != null){
+            CreateID.removeTicketID(ticket.getId());
+            if (ticket.getVenue()!=null){
+                CreateID.removeVenueID(ticket.getVenue().getID());
+            }
+            collection.pollFirst();
+        }
+    }
+
+    public boolean removeById(int id){
+        Iterator<Ticket> iterator = collection.iterator();
+        while (iterator.hasNext()){
+            Ticket ticket = iterator.next();
+            if (id == ticket.getId()){
+                iterator.remove();
+                CreateID.removeTicketID(id);
+                if (ticket.getVenue()!=null){
+                    CreateID.removeVenueID(ticket.getVenue().getID());
+                }
+                sortCollection();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int removeAllByPrice(Long price){
+        Iterator<Ticket> iterator = collection.iterator();
+        int count = 0;
+        while (iterator.hasNext()){
+            Ticket ticket = iterator.next();
+            if (price.equals(ticket.getPrice())){
+                count = count +1;
+                iterator.remove();
+                CreateID.removeTicketID(ticket.getId());
+                if (ticket.getVenue()!=null){
+                    CreateID.removeVenueID(ticket.getVenue().getID());
+                }
+            }
+        }
+        sortCollection();
+        return count;
+    }
+
+    public boolean removeByType(TicketType type){
+        Iterator<Ticket> iterator = collection.iterator();
+        while (iterator.hasNext()){
+            Ticket ticket = iterator.next();
+            if (type == ticket.getType()){
+                iterator.remove();
+                CreateID.removeTicketID(ticket.getId());
+                if (ticket.getVenue()!=null){
+                    CreateID.removeVenueID(ticket.getVenue().getID());
+                }
+                sortCollection();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int removeAllGreater(Ticket newTicket){
+        Iterator<Ticket> iterator = collection.iterator();
+        int count = 0;
+        while (iterator.hasNext()){
+            Ticket ticket = iterator.next();
+            if (ticket.compareTo(newTicket)<0){
+                count = count +1;
+                iterator.remove();
+                CreateID.removeTicketID(ticket.getId());
+                if (ticket.getVenue()!=null){
+                    CreateID.removeVenueID(ticket.getVenue().getID());
+                }
+            } else{
+                break;
+            }
+        }
+        sortCollection();
+        return count;
+    }
+
+    public Ticket getMinByVenue() {
+        return collection.stream().min(Comparator.comparing(Ticket::getVenue,
+                Comparator.nullsFirst(Comparator.naturalOrder()))).orElse(null);
+    }
+}
