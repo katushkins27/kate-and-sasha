d2ca3281e6bdb10ac168bcb707c0d509e770a1f0
katushkins27 <sergeenkovaev@gmail.com>
2026-04-20 21:21:18 +0300

    окончательный вариант команды

--- server/commands/RemoveAllByPriceCommand.java
+++ server/commands/RemoveAllByPriceCommand.java
@@ -5,24 +5,29 @@ import server.TicketCollection;
 import common.data.Ticket;
 
 public class RemoveAllByPriceCommand implements Command {
+    private TicketCollection collection;
+
+    private Ticket findTicketByPrice(Long price) {
+        for (Ticket ticket : collection.getCollection()) {
+            Long ticketPrice = ticket.getPrice();
+            if (price == null && ticketPrice == null) {
+                return ticket;
+            }
+            if (price != null && price.equals(ticketPrice)) {
+                return ticket;
+            }
+        }
+        return null;
+    }
 
     @Override
     public Response execute(TicketCollection collection, String arg, Object extraData) {
-        Long price = null;
-        if(extraData instanceof Long){
-            price = (Long) extraData;
-        } else if (arg!=null && !arg.isEmpty()){
-            try{
-                price=Long.parseLong(arg);
-            } catch (NumberFormatException e){
-                return new Response(false, "Ошибка в цене. Введите число");
-            }
-        }
-        int removed = collection.removeAllByPrice(price);
-        if (removed==0){
-            return new Response(true, "Билеты с ценой "+price+" не найдены");
-        } else {
-            return new Response(true, "Билеты удалены. Количество удаленных билетов: "+removed);
+        try {
+            Long price = (Long) extraData;
+            int removed = collection.removeAllByPrice(price);
+            return new Response(true, "Билеты удалены " + removed);
+        } catch (NumberFormatException e) {
+            return new Response(false, "Ошибка в цене. Введите число");
         }
     }
 
