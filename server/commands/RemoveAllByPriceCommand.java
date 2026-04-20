4728979f3ac212c0ec1892f44c70fc55ad6127a0
aleksandraTikhomirova <sashatihomirova16@gmail.com>
2026-04-20 18:38:32 +0300

    Update RemoveAllByPriceCommand.java

--- server/commands/RemoveAllByPriceCommand.java
+++ server/commands/RemoveAllByPriceCommand.java
@@ -5,29 +5,24 @@ import server.TicketCollection;
 import common.data.Ticket;
 
 public class RemoveAllByPriceCommand implements Command {
-    private TicketCollection collection;
-
-    private Ticket findTicketByPrice(Long price) {
-        for (Ticket ticket : collection.getCollection()) {
-            Long ticketPrice = ticket.getPrice();
-            if (price == null && ticketPrice == null) {
-                return ticket;
-            }
-            if (price != null && price.equals(ticketPrice)) {
-                return ticket;
-            }
-        }
-        return null;
-    }
 
     @Override
     public Response execute(TicketCollection collection, String arg, Object extraData) {
-        try {
-            Long price = (arg == null || arg.isEmpty()) ? null : Long.parseLong(arg);
-            collection.removeAllByPrice(price);
-            return new Response(true, "Билеты удалены");
-        } catch (NumberFormatException e) {
-            return new Response(false, "Ошибка в цене. Введите число");
+        Long price = null;
+        if(extraData instanceof Long){
+            price = (Long) extraData;
+        } else if (arg!=null && !arg.isEmpty()){
+            try{
+                price=Long.parseLong(arg);
+            } catch (NumberFormatException e){
+                return new Response(false, "Ошибка в цене. Введите число");
+            }
+        }
+        int removed = collection.removeAllByPrice(price);
+        if (removed==0){
+            return new Response(true, "Билеты с ценой "+price+" не найдены");
+        } else {
+            return new Response(true, "Билеты удалены. Количество удаленных билетов: "+removed);
         }
     }
 
